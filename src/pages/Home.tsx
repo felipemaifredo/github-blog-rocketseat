d4851875e4e54ecdb4a1fc42cb1e42b44ab88e88
felipemaifredo <felipemaifredo@gmail.com>
2024-04-28 03:08:33 -0300

    att

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,15 @@
 import { BannerInitial } from "../sections/BannerInitial"
 import { Profile } from "../sections/Profile"
 import { SearchSection } from "../sections/SearchSection"
+import { IssuesSection } from "../sections/IssuesSection"
+
 export const Home = () => {
     return(
         <>
             <BannerInitial />
             <Profile />
             <SearchSection />
+            <IssuesSection />
         </>
     )
 }
